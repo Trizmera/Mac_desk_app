626e87113f8fdaf432b47a98343f1058a3900f5d
trizmera <mllebia@live.com>
2024-01-26 13:40:11 -0300

    Initial Commit

---
+++ mac_desk_app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mac_desk_app
+//
+//  Created by MAC on 26/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mac_desk_app/mac_desk_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_desk_appApp.swift
+//  mac_desk_app
+//
+//  Created by MAC on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct mac_desk_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
